89d7491faaff6f97b285d2e0f6db82f18b1af104
Vidhi Nagda <nagda.v@northeastern.edu>
2021-04-25 16:03:26 -0400

    Build

--- uberfrontend/src/components/url.js
+++ uberfrontend/src/components/url.js
@@ -1,3 +1,3 @@
-export const python_url = `http://a98b9b8d0c5e0427cb72f5d70105f08d-242657450.us-west-2.elb.amazonaws.com:5000/app/`;
+export const python_url = `http://a98b9b8d0c5e0427cb72f5d70105f08d-242657450.us-west-2.elb.amazonaws.com:5000/`;
 
 export const react_url = `http://localhost:5000/app/`;
