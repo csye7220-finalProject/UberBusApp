9aea83a84065e79ebbe7080b827fe9e51729dc34
Vidhi Nagda <nagda.v@northeastern.edu>
2021-04-25 02:20:12 -0400

    Build and url changes

--- uberfrontend/src/components/AccessApiCalls.js
+++ uberfrontend/src/components/AccessApiCalls.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import "./url";
+import { python_url } from "./url.js";
+// import { react_url } from "./url.js";
+
 const url = python_url;
 // const url = "/app/";
 // const url = "http://localhost:5000/app/";
--- uberfrontend/src/components/url.js
+++ uberfrontend/src/components/url.js
@@ -1,1 +1,3 @@
-const python_url = `http://ad5f8402a31d1430ba79fe66b89f65be-1681252358.us-west-2.elb.amazonaws.com:5000/app/`;
+export const python_url = `http://ad5f8402a31d1430ba79fe66b89f65be-1681252358.us-west-2.elb.amazonaws.com:5000/app/`;
+
+export const react_url = `http://localhost:5000/app/`;
